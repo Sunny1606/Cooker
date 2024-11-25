29f2a2b9854dc937efbf88de63cf9c175fffd249
Sunny1606 <solescobares@gmail.com>
2024-11-25 19:27:24 -0300

    done

--- app/cooker/src/App.jsx
+++ app/cooker/src/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Home from "./components/Home/Home";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Chefs from "./components/Chefs/index";
 
 function App() {
   return (
     <>
       <Nav />
-      <Home />
-      <Footer/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chefs" element={<Chefs />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- app/cooker/src/components/Nav/Nav.jsx
+++ app/cooker/src/components/Nav/Nav.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import style from "./Nav.module.css";
-import image from '/src/images/chef-logo-chefs-hat-knives-600nw-1691316280.jpg'
+import image from "/src/images/chef-logo-chefs-hat-knives-600nw-1691316280.jpg";
+import { Link } from "react-router-dom";
+
 
 
 export default function Nav() {
   return (
     <div className={style.conteinerBtn}>
-    <img className={style.img} src={image} alt="" />
-      <button className={style.btn}>CHEFS</button>
-      <button className={style.btn}>CHAU</button>
+      <img className={style.img} src={image} alt="" />
+      <Link to={"/chefs"}>
+        <button className={style.btn}>CHEFS</button>
+      </Link>
+
+      {/* <button className={style.btn}>CHAU</button> */}
+
       {/* <button className={style.btn}>QUE TAL</button>
       <button className={style.btn}>ADIOS</button> */}
     </div>
